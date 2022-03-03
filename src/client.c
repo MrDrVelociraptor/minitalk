53e377e1eb0c79fe2c4893c7ec5b7fd55e83f825
Nicholas Speedy <nspeedy@F3C028.42adel.org.au>
2022-03-03 15:33:42 +1030

    Bonus complete

--- src/client.c
+++ src/client.c
@@ -12,6 +12,24 @@
 
 #include "minitalk.h"
 
+/* This function recieves a signal from the server after
+message has been sent and recieved*/
+
+static void	response(int sig)
+{
+	(void)sig;
+	if (sig == SIGUSR1)
+	{
+		ft_printf("The server acknowledges you\n");
+		exit(0);
+	}
+}
+
+/* This function sends a char to the server. By bit shifting through
+each char and sending SIGURSR1 if the current bit is 1(on) - (c >> shift & 1)
+or SIGUSR2 if the bit is 0(off) then shift decreases and the next
+bit is checked*/
+
 static void	bit_send(int pid, char c)
 {
 	int	shift;
@@ -37,20 +55,27 @@ static void	bit_send(int pid, char c)
 int	main(int ac, char *arg[])
 {
 	int					i;
-	
+	struct sigaction	cdata;
+
+	cdata.sa_handler = &response;
+	sigemptyset(&cdata.sa_mask);
 	if (ac != 3)
 	{
-		printf("Correct input ./client <server-pid> <message>\n");
+		ft_printf("Correct input ./client <server-pid> <message>\n");
 		return (1);
 	}
 	i = 0;
 	while (1)
 	{
+		sigaction(SIGUSR1, &cdata, 0);
 		while (arg[2][i] != '\0')
 		{
-		bit_send(ft_atoi(arg[1]), arg[2][i]);
-		i++;
+			bit_send(ft_atoi(arg[1]), arg[2][i]);
+			i++;
 		}
+		if (arg[2][i] == '\0')
+			bit_send(ft_atoi(arg[1]), arg[2][i]);
+		pause();
 	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -20,23 +20,21 @@ static void	print_pid(void)
 	ft_printf("Run the client with ./client <PID> <message>\n");
 }
 
-static void	handle(int sig)
+static void	handle(int sig, siginfo_t *cl_pid, void *horse)
 {
 	static unsigned char	byte;
 	static int				shift;
 
+	(void)horse;
 	byte <<= 1;
 	shift++;
 	if (sig == SIGUSR1)
-	{
-		signal(SIGUSR1, handle);
 		byte++;
-	}
-	else if (sig == SIGUSR2)
-		signal(SIGUSR2, handle);
 	if (shift == 8)
 	{
 		write(1, &byte, 1);
+		if (!byte)
+			kill(cl_pid->si_pid, SIGUSR1);
 		byte = 0;
 		shift = 0;
 	}
@@ -44,9 +42,16 @@ static void	handle(int sig)
 
 int	main(void)
 {
+	struct sigaction	sdata;
+
+	sigemptyset(&sdata.sa_mask);
+	sdata.sa_flags = SA_SIGINFO;
+	sdata.sa_sigaction = &handle;
 	print_pid();
-	signal(SIGUSR1, handle);
-	signal(SIGUSR2, handle);
 	while (1)
+	{
+		sigaction(SIGUSR1, &sdata, 0);
+		sigaction(SIGUSR2, &sdata, 0);
 		pause();
+	}
 }
